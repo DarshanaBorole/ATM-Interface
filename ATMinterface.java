041904ea62783111fc23a498e3e72432699b80da
Darshana Borole <134164242+DarshanaBorole@users.noreply.github.com>
2024-04-16 20:11:10 +0530

    ATMinterface.java

---
+++ ATMinterface.java
@@ -0,0 +1,12 @@
+package com.mycompany.atminterface;
+
+/**
+ *
+ * @author Dell
+ */
+public class ATMinterface {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
